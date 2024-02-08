d0205dc78e72feddb645d46d5f438d148d57c7f6
Nicholas Murray <nmurr13@protonmail.com>
2024-02-07 19:04:43 -0800

    backend ready to deploy

---
+++ models/cheese.js
@@ -0,0 +1,11 @@
+const mongoose = require('./connection');
+
+const CheeseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String
+});
+
+const Cheese = mongoose.model('Cheese', CheeseSchema);
+
+module.exports = Cheese;
